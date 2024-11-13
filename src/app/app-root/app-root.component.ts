1d31ad33f048940cbd98e605cd99e47d9f0a9b46
pabloaltair <csi24-prosdez@colegioaltair.net>
2024-11-13 12:55:31 +0100

    Version 1.0

---
+++ src/app/app-root/app-root.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-root.component.html',
+  styleUrl: './app-root.component.css'
+})
+export class AppRootComponent {
+
+}
---
+++ src/app/tarea/tarea.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarea',
+  standalone: true,
+  imports: [],
+  templateUrl: './tarea.component.html',
+  styleUrl: './tarea.component.css'
+})
+export class TareaComponent {
+  
+}
